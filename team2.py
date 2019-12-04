02735f4ce21870779e4c451013466558e4dd4aa3
maggienardini <58440746+maggienardini@users.noreply.github.com>
2019-12-04 13:22:20 -0500

    Update team2.py

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 2' # Only 10 chars displayed.
+strategy_name = 'Collude then always betray'
+strategy_description = 'Collude on the first turn then betray on the rest of the turns'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,10 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    else:
+        return 'b' # otherwise collude.
 
     
-def test_move(my_history, their_history, my_score, their_score, result):
+''' def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
@@ -65,4 +68,5 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')        
+              '''
